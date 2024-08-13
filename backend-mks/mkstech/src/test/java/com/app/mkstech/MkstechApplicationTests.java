a10a93207dc3e04c8be5b18193a6e83bd9e8ba9b
robbintj <robbintj@hotmail.com>
2024-08-13 16:01:16 -0300

    atualizar readme

---
+++ backend-mks/mkstech/src/test/java/com/app/mkstech/MkstechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mkstech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkstechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
