7b94488dd4cf0c82f068bf28e2992f1dc28f0772
robbintj <robbintj@hotmail.com>
2024-08-13 20:53:56 -0300

    implementar getters, setters e ajuste para prod do hibernate

--- backend-mks/mkstech/src/main/java/com/app/mkstech/model/Overlay.java
+++ backend-mks/mkstech/src/main/java/com/app/mkstech/model/Overlay.java
@@ -38,8 +38,263 @@ public class Overlay {
     private String observacaoMKS;
     private String observacaoParceiro;
 
-    public void setId(Long id) {
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
     }
 
-    // Getters and Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(String idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getParedeFornalha() {
+        return paredeFornalha;
+    }
+
+    public void setParedeFornalha(String paredeFornalha) {
+        this.paredeFornalha = paredeFornalha;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public int getNumeroTubo() {
+        return numeroTubo;
+    }
+
+    public void setNumeroTubo(int numeroTubo) {
+        this.numeroTubo = numeroTubo;
+    }
+
+    public String getNumeroTuboAdjacente() {
+        return numeroTuboAdjacente;
+    }
+
+    public void setNumeroTuboAdjacente(String numeroTuboAdjacente) {
+        this.numeroTuboAdjacente = numeroTuboAdjacente;
+    }
+
+    public int getElevacaoInferior() {
+        return elevacaoInferior;
+    }
+
+    public void setElevacaoInferior(int elevacaoInferior) {
+        this.elevacaoInferior = elevacaoInferior;
+    }
+
+    public int getElevacaoSuperior() {
+        return elevacaoSuperior;
+    }
+
+    public void setElevacaoSuperior(int elevacaoSuperior) {
+        this.elevacaoSuperior = elevacaoSuperior;
+    }
+
+    public int getDimensao() {
+        return dimensao;
+    }
+
+    public void setDimensao(int dimensao) {
+        this.dimensao = dimensao;
+    }
+
+    public String getEscopo() {
+        return escopo;
+    }
+
+    public void setEscopo(String escopo) {
+        this.escopo = escopo;
+    }
+
+    public String getLado() {
+        return lado;
+    }
+
+    public void setLado(String lado) {
+        this.lado = lado;
+    }
+
+    public String getTipoEscopo() {
+        return tipoEscopo;
+    }
+
+    public void setTipoEscopo(String tipoEscopo) {
+        this.tipoEscopo = tipoEscopo;
+    }
+
+    public String getLiberadoGeral() {
+        return liberadoGeral;
+    }
+
+    public void setLiberadoGeral(String liberadoGeral) {
+        this.liberadoGeral = liberadoGeral;
+    }
+
+    public String getPendenteGeralParceiro() {
+        return pendenteGeralParceiro;
+    }
+
+    public void setPendenteGeralParceiro(String pendenteGeralParceiro) {
+        this.pendenteGeralParceiro = pendenteGeralParceiro;
+    }
+
+    public String getPendenteGeralMKS() {
+        return pendenteGeralMKS;
+    }
+
+    public void setPendenteGeralMKS(String pendenteGeralMKS) {
+        this.pendenteGeralMKS = pendenteGeralMKS;
+    }
+
+    public int getExecutadoSoldaMKS() {
+        return executadoSoldaMKS;
+    }
+
+    public void setExecutadoSoldaMKS(int executadoSoldaMKS) {
+        this.executadoSoldaMKS = executadoSoldaMKS;
+    }
+
+    public Date getTerminoMKS() {
+        return terminoMKS;
+    }
+
+    public void setTerminoMKS(Date terminoMKS) {
+        this.terminoMKS = terminoMKS;
+    }
+
+    public int getValidacaoCQMKS() {
+        return validacaoCQMKS;
+    }
+
+    public void setValidacaoCQMKS(int validacaoCQMKS) {
+        this.validacaoCQMKS = validacaoCQMKS;
+    }
+
+    public Date getValidacaoMKS() {
+        return validacaoMKS;
+    }
+
+    public void setValidacaoMKS(Date validacaoMKS) {
+        this.validacaoMKS = validacaoMKS;
+    }
+
+    public int getValidadoParceiro() {
+        return validadoParceiro;
+    }
+
+    public void setValidadoParceiro(int validadoParceiro) {
+        this.validadoParceiro = validadoParceiro;
+    }
+
+    public int getVsParceiro() {
+        return vsParceiro;
+    }
+
+    public void setVsParceiro(int vsParceiro) {
+        this.vsParceiro = vsParceiro;
+    }
+
+    public int getLpParceiro() {
+        return lpParceiro;
+    }
+
+    public void setLpParceiro(int lpParceiro) {
+        this.lpParceiro = lpParceiro;
+    }
+
+    public String getLiberadoParceiro() {
+        return liberadoParceiro;
+    }
+
+    public void setLiberadoParceiro(String liberadoParceiro) {
+        this.liberadoParceiro = liberadoParceiro;
+    }
+
+    public Date getDataLiberadoParceiro() {
+        return dataLiberadoParceiro;
+    }
+
+    public void setDataLiberadoParceiro(Date dataLiberadoParceiro) {
+        this.dataLiberadoParceiro = dataLiberadoParceiro;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getObservacaoAlumar() {
+        return observacaoAlumar;
+    }
+
+    public void setObservacaoAlumar(String observacaoAlumar) {
+        this.observacaoAlumar = observacaoAlumar;
+    }
+
+    public String getObservacaoMKS() {
+        return observacaoMKS;
+    }
+
+    public void setObservacaoMKS(String observacaoMKS) {
+        this.observacaoMKS = observacaoMKS;
+    }
+
+    public String getObservacaoParceiro() {
+        return observacaoParceiro;
+    }
+
+    public void setObservacaoParceiro(String observacaoParceiro) {
+        this.observacaoParceiro = observacaoParceiro;
+    }
+
+    @Override
+    public String toString() {
+        return "Overlay{" +
+                "id=" + id +
+                ", idProduto='" + idProduto + '\'' +
+                ", paredeFornalha='" + paredeFornalha + '\'' +
+                ", local='" + local + '\'' +
+                ", numeroTubo=" + numeroTubo +
+                ", numeroTuboAdjacente='" + numeroTuboAdjacente + '\'' +
+                ", elevacaoInferior=" + elevacaoInferior +
+                ", elevacaoSuperior=" + elevacaoSuperior +
+                ", dimensao=" + dimensao +
+                ", escopo='" + escopo + '\'' +
+                ", lado='" + lado + '\'' +
+                ", tipoEscopo='" + tipoEscopo + '\'' +
+                ", liberadoGeral='" + liberadoGeral + '\'' +
+                ", pendenteGeralParceiro='" + pendenteGeralParceiro + '\'' +
+                ", pendenteGeralMKS='" + pendenteGeralMKS + '\'' +
+                ", executadoSoldaMKS=" + executadoSoldaMKS +
+                ", terminoMKS=" + terminoMKS +
+                ", validacaoCQMKS=" + validacaoCQMKS +
+                ", validacaoMKS=" + validacaoMKS +
+                ", validadoParceiro=" + validadoParceiro +
+                ", vsParceiro=" + vsParceiro +
+                ", lpParceiro=" + lpParceiro +
+                ", liberadoParceiro='" + liberadoParceiro + '\'' +
+                ", dataLiberadoParceiro=" + dataLiberadoParceiro +
+                ", status='" + status + '\'' +
+                ", observacaoAlumar='" + observacaoAlumar + '\'' +
+                ", observacaoMKS='" + observacaoMKS + '\'' +
+                ", observacaoParceiro='" + observacaoParceiro + '\'' +
+                '}';
+    }
 }
