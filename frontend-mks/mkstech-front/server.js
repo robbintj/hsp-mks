ec96773ee3501c5a2b0b485c9fb884350a167ebf
robbintj <robbintj@hotmail.com>
2024-08-19 15:40:35 -0300

    atualizar pasta static

--- frontend-mks/mkstech-front/server.js
+++ frontend-mks/mkstech-front/server.js
@@ -2,19 +2,36 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve os arquivos estáticos da pasta 'dist/mkstech-front'
-app.use(express.static(path.join(__dirname, 'dist/mkstech-front/browser')));
+// Serve apenas os arquivos estáticos na pasta dist
+app.use(express.static(__dirname + '/dist/mkstech-front'));
 
-// Roteia todas as solicitações para o arquivo index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/mkstech-front/browser/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/mkstech-front/index.html'));
 });
 
-// Define a porta que o servidor irá ouvir
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+// Inicia a aplicação pela porta padrão do Heroku
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Servidor iniciado na porta ' + (process.env.PORT || 8080));
 });
+
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// // Serve os arquivos estáticos da pasta 'dist/mkstech-front'
+// app.use(express.static(path.join(__dirname, 'dist/mkstech-front/browser')));
+
+// // Roteia todas as solicitações para o arquivo index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist/mkstech-front/browser/index.html'));
+// });
+
+// // Define a porta que o servidor irá ouvir
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Servidor rodando na porta ${port}`);
+// });
 
 
 
