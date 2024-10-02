bee6dcca770a2fe32ab5bd9dc9b5574e71d40e97
Robert Rodrigues <robbintj@hotmail.com>
2024-10-02 13:05:52 -0300

    Update WebConfig.java
    url aws

--- backend-mks/mkstech/src/main/java/com/app/mkstech/config/WebConfig.java
+++ backend-mks/mkstech/src/main/java/com/app/mkstech/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Permitir apenas a origem do frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+               // .allowedOrigins("https://mksteh.s3-website-sa-east-1.amazonaws.com") // URL pública do frontend
+                .allowedOrigins("http://localhost:4200") // Permitir apenas a origem do frontend mks
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*");
     }
 }
