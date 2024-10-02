f5a76944ee9e7f47418a4cb2d19ef9c1cd5b5289
robbintj <robbintj@hotmail.com>
2024-10-02 12:35:29 -0300

    ajuste no webconfig url do front

--- src/main/java/com/app/mkstech/config/WebConfig.java
+++ src/main/java/com/app/mkstech/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://mksteh.s3-website-sa-east-1.amazonaws.com") // URL pública do frontend
-//                .allowedOrigins("http://localhost:4200") // Permitir apenas a origem do frontend
+               // .allowedOrigins("https://mksteh.s3-website-sa-east-1.amazonaws.com") // URL pública do frontend
+                .allowedOrigins("http://localhost:4200") // Permitir apenas a origem do frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*");
     }
